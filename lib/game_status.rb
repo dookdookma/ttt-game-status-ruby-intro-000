5c4611906f1a59b3b24b5a2a9d5b509d4ea7bd2b
Luke Rollins <ldrollins@gmail.com>
2021-02-19 23:21:41 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -38,5 +38,5 @@ def over?(board)
 end
 
 def winner(board)
-  WIN_COMBINATIONS.detect{|combo| combo.is_a?("X") || combo.is_a?("O")}
+  WIN_COMBINATIONS.detect{|combo| combo[0].is_a?("X") || combo[0].is_a?("O")}
 end
